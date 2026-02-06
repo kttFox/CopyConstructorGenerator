c021b11976f0fd536911bcc5a0320401052c270f
kttFox <kttFoxOnFire@gmail.com>
2026-02-07 05:16:34 +0900

    [test] テストコード作成

---
+++ CopyConstructorGenerator.Test/UnitTest_JP.cs
@@ -0,0 +1,243 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CopyConstructorGenerator.Test;
+
+using Verify = CSharpCodeFixVerifier<CopyConstructorGeneratorAnalyzer, CopyConstructorGeneratorCodeFixProvider>;
+
+[TestClass]
+public class UnitTest_JP {
+	[TestInitialize]
+	public void TestInitialize() {
+		Thread.CurrentThread.CurrentUICulture = new CultureInfo( "ja-JP" );
+		Thread.CurrentThread.CurrentCulture = new CultureInfo( "ja-JP" );
+	}
+
+
+	//No diagnostics expected to show up
+	[TestMethod]
+	public async Task Nothing() {
+		var test = @"";
+
+		await Verify.VerifyAnalyzerAsync( test );
+	}
+
+
+	[TestMethod]
+	public async Task TestMethod1() {
+		var testCode = """
+		               class A {
+		               }
+
+		               """;
+
+		var fixTest = """
+		              class A {
+		                  /// <summary>
+		                  /// コピーコンストラクター
+		                  /// </summary>
+		                  public A(A value)
+		                  {
+		              
+		                  }
+		              }
+
+		              """;
+
+		var expected = Verify.Diagnostic( "CopyConstructorGenerator" ).WithLocation( 1, 7 );
+
+		var test = new Verify.Test() {
+			TestCode = testCode,
+			FixedCode = fixTest,
+			ExpectedDiagnostics = { expected },
+			FixedState = { ExpectedDiagnostics = { expected } },
+		};
+
+		await test.RunAsync( CancellationToken.None );
+	}
+
+	[TestMethod]
+	public async Task TestMethod2() {
+		var testCode = """
+			class A {
+			    public int X { get; set; }
+			}
+
+			""";
+
+		var fixTest = """
+			class A {
+			    /// <summary>
+			    /// コピーコンストラクター
+			    /// </summary>
+			    public A(A value)
+			    {
+			        this.X = value.X;
+			    }
+
+			    public int X { get; set; }
+			}
+
+			""";
+
+		var expected = Verify.Diagnostic( "CopyConstructorGenerator" ).WithLocation( 1, 7 );
+
+		var test = new Verify.Test() {
+			TestCode = testCode,
+			FixedCode = fixTest,
+			ExpectedDiagnostics = { expected },
+			FixedState = { ExpectedDiagnostics = { expected } },
+		};
+
+		await test.RunAsync( CancellationToken.None );
+	}
+
+	[TestMethod]
+	public async Task TestMethod3() {
+		var testCode = """
+				class A {
+					public A()
+					{
+						
+					}
+
+					public int X { get; set; }
+				}
+				""";
+
+		var fixTest = """
+				class A {
+					/// <summary>
+					/// コピーコンストラクター
+					/// </summary>
+					public A(A value)
+					{
+						this.X = value.X;
+					}
+
+					public A()
+					{
+						
+					}
+
+					public int X { get; set; }
+				}
+				""";
+
+		var expected = Verify.Diagnostic( "CopyConstructorGenerator" ).WithLocation( 1, 7 );
+
+		var test = new Verify.Test() {
+			TestCode = testCode,
+			FixedCode = fixTest,
+			ExpectedDiagnostics = { expected },
+			FixedState = { ExpectedDiagnostics = { expected } },
+			TestState = {
+				AnalyzerConfigFiles = {
+					("/.editorconfig", """
+						root = true
+
+						[*.cs]
+						indent_style = tab
+						indent_size = 4
+						""")
+				}
+			}
+		};
+
+		await test.RunAsync( CancellationToken.None );
+	}
+
+	/// <summary>
+	/// 既存のコピーコンストラクターがある場合でも、先頭に追加される
+	/// 複数のコピーコンストラクターが存在することになる
+	/// </summary>
+	/// <returns></returns>
+	[TestMethod]
+	public async Task TestMethod3_ExistingCopyConstructor() {
+		var testCode = """
+		class A {
+		    public A(A value) {
+		        // 既存のコピーコンストラクター
+		    }
+		    
+		    public int X { get; set; }
+		}
+
+		""";
+
+		var fixTest = """
+		class A {
+		    /// <summary>
+		    /// コピーコンストラクター
+		    /// </summary>
+		    public A(A value)
+		    {
+		        this.X = value.X;
+		    }
+
+		    public A(A value) {
+		        // 既存のコピーコンストラクター
+		    }
+		    
+		    public int X { get; set; }
+		}
+
+		""";
+
+		var expected = Verify.Diagnostic( "CopyConstructorGenerator" ).WithLocation( 1, 7 );
+
+		var test = new Verify.Test() {
+			TestCode = testCode,
+			FixedCode = fixTest,
+			ExpectedDiagnostics = { expected },
+			FixedState = { ExpectedDiagnostics = { expected, DiagnosticResult.CompilerError( "CS0111" ).WithSpan( 10, 12, 10, 13 ).WithArguments( "A", "A" ), } },
+			CodeFixTestBehaviors = CodeFixTestBehaviors.FixOne,
+
+		};
+
+		await test.RunAsync( CancellationToken.None );
+	}
+
+	[TestMethod]
+	public async Task TestMethod4_PropertiesOnlyCodeFix() {
+		var testCode = """
+			class A {
+			    public int X { get; set; }
+			    private int num = 10;
+			}
+
+			""";
+
+		var fixTest = """
+			class A {
+			    /// <summary>
+			    /// コピーコンストラクター
+			    /// </summary>
+			    public A(A value)
+			    {
+			        this.X = value.X;
+			    }
+
+			    public int X { get; set; }
+			    private int num = 10;
+			}
+
+			""";
+
+		var expected = Verify.Diagnostic( "CopyConstructorGenerator" ).WithLocation( 1, 7 );
+
+		var test = new Verify.Test() {
+			TestCode = testCode,
+			FixedCode = fixTest,
+			ExpectedDiagnostics = { expected },
+			FixedState = { ExpectedDiagnostics = { expected } },
+			CodeActionIndex = 1,  // プロパティのみのコードフィックス（2番目）を選択
+			CodeFixTestBehaviors = CodeFixTestBehaviors.FixOne | CodeFixTestBehaviors.SkipFixAllCheck,
+		};
+
+		await test.RunAsync( CancellationToken.None );
+	}
+}
--- CopyConstructorGenerator.Test/Verifiers/CSharpCodeFixVerifier`2.cs
+++ CopyConstructorGenerator.Test/Verifiers/CSharpCodeFixVerifier`2.cs
@@ -5,7 +5,6 @@ using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Testing;
-using Microsoft.CodeAnalysis.Testing.Verifiers;
 
 namespace CopyConstructorGenerator.Test {
 	public static partial class CSharpCodeFixVerifier<TAnalyzer, TCodeFix>
@@ -13,42 +12,40 @@ namespace CopyConstructorGenerator.Test {
 		where TCodeFix : CodeFixProvider, new() {
 		/// <inheritdoc cref="CodeFixVerifier{TAnalyzer, TCodeFix, TTest, TVerifier}.Diagnostic()"/>
 		public static DiagnosticResult Diagnostic()
-			=> CSharpCodeFixVerifier<TAnalyzer, TCodeFix, MSTestVerifier>.Diagnostic();
+			=> CSharpCodeFixVerifier<TAnalyzer, TCodeFix, DefaultVerifier>.Diagnostic();
 
 		/// <inheritdoc cref="CodeFixVerifier{TAnalyzer, TCodeFix, TTest, TVerifier}.Diagnostic(string)"/>
 		public static DiagnosticResult Diagnostic( string diagnosticId )
-			=> CSharpCodeFixVerifier<TAnalyzer, TCodeFix, MSTestVerifier>.Diagnostic( diagnosticId );
+			=> CSharpCodeFixVerifier<TAnalyzer, TCodeFix, DefaultVerifier>.Diagnostic( diagnosticId );
 
 		/// <inheritdoc cref="CodeFixVerifier{TAnalyzer, TCodeFix, TTest, TVerifier}.Diagnostic(DiagnosticDescriptor)"/>
 		public static DiagnosticResult Diagnostic( DiagnosticDescriptor descriptor )
-			=> CSharpCodeFixVerifier<TAnalyzer, TCodeFix, MSTestVerifier>.Diagnostic( descriptor );
+			=> CSharpCodeFixVerifier<TAnalyzer, TCodeFix, DefaultVerifier>.Diagnostic( descriptor );
 
 		/// <inheritdoc cref="CodeFixVerifier{TAnalyzer, TCodeFix, TTest, TVerifier}.VerifyAnalyzerAsync(string, DiagnosticResult[])"/>
-		public static async Task VerifyAnalyzerAsync( string source, params DiagnosticResult[] expected ) {
+		public static async Task VerifyAnalyzerAsync( string source ) {
 			var test = new Test {
 				TestCode = source,
 			};
 
-			test.ExpectedDiagnostics.AddRange( expected );
 			await test.RunAsync( CancellationToken.None );
 		}
 
-		/// <inheritdoc cref="CodeFixVerifier{TAnalyzer, TCodeFix, TTest, TVerifier}.VerifyCodeFixAsync(string, string)"/>
-		public static async Task VerifyCodeFixAsync( string source, string fixedSource )
-			=> await VerifyCodeFixAsync( source, DiagnosticResult.EmptyDiagnosticResults, fixedSource );
-
 		/// <inheritdoc cref="CodeFixVerifier{TAnalyzer, TCodeFix, TTest, TVerifier}.VerifyCodeFixAsync(string, DiagnosticResult, string)"/>
-		public static async Task VerifyCodeFixAsync( string source, DiagnosticResult expected, string fixedSource )
-			=> await VerifyCodeFixAsync( source, new[] { expected }, fixedSource );
+		public static async Task VerifyCodeFixAsync( string source, DiagnosticResult expected, string fixedSource, DiagnosticResult fixedStateExpected = default ) {
+			await VerifyCodeFixAsync( source, [expected], fixedSource, [fixedStateExpected] );
+		}
 
 		/// <inheritdoc cref="CodeFixVerifier{TAnalyzer, TCodeFix, TTest, TVerifier}.VerifyCodeFixAsync(string, DiagnosticResult[], string)"/>
-		public static async Task VerifyCodeFixAsync( string source, DiagnosticResult[] expected, string fixedSource ) {
+		public static async Task VerifyCodeFixAsync( string source, DiagnosticResult[] expected, string fixedSource, DiagnosticResult[] fixedStateExpected ) {
 			var test = new Test {
 				TestCode = source,
 				FixedCode = fixedSource,
 			};
 
 			test.ExpectedDiagnostics.AddRange( expected );
+			test.FixedState.ExpectedDiagnostics.AddRange( fixedStateExpected );
+
 			await test.RunAsync( CancellationToken.None );
 		}
 	}
